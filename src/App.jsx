c7add0910af48a0c22795f8126272e2bbf1312ba
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-09-17 00:23:49 +0545

    Add routing and contact page with styling
    - Implemented React Router for navigation
    - Created Contact component with a link to the home page
    - Added CSS styling for the contact title
    - Updated package.json and package-lock.json to include react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Contact from "./Contact";
+
 export default function App() {
   return (
-<>
-  <h1>Hello, world!</h1>
-</>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Hello World!</h1>} />
+          <Route path="/about" element={<h1>About Us</h1>} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
---
+++ src/Contact.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+import "./home.css"
+const Contact = () => {
+  return (
+    <div>
+      <h1 className="contact-title">Contact Us</h1>
+      <Link to="/">Go to Home</Link>
+    </div>
+  )
+}
+
+export default Contact
